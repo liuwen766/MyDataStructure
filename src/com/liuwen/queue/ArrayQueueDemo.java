85cffa5c4dc13b867fb8468db0ff1c5e58945cb7
liuwen766 <780442619@qq.com>
2020-02-16 15:02:38 +0800

    数据结构学习第一阶段

---
+++ src/com/liuwen/queue/ArrayQueueDemo.java
@@ -0,0 +1,24 @@
+package com.liuwen.queue;
+
+public class ArrayQueueDemo {
+    public static void main(String[] args){
+
+    }
+
+    // 使用数组模拟队列-编写一个ArrayQueue类
+
+    // front指向队列头部，分析出front是指向队列头的前一个位置.
+    // rear指向队列尾，指向队列尾的数据(即就是队列最后一个数据)
+
+    // 判断队列是否满
+
+    // 判断队列是否为空
+
+    // 添加数据到队列
+
+    // 获取队列的数据, 出队列
+
+    // 显示队列的所有数据
+
+    // 显示队列的头数据， 注意不是取出数据
+}
